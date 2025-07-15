84b7a69c2b208a579c2b0bfa6ac1e71ea441ce71
gerry leo nugroho <gerryleonugroho@gmail.com>
2025-07-15 17:12:13 +0700

    fix(rss): correct blog post links in RSS feed
    The links were incorrectly prefixed with '/blog/' which doesn't match the actual URL structure. Changed to use the correct path prefix.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,11 +12,7 @@ export async function GET(context) {
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-<<<<<<< HEAD
-      link: `/blog/${post.slug}/`,
-=======
       link: `/${post.slug}/`, // Corrected link
->>>>>>> dev
     })),
   });
 }
